335d4773bcf19b9c4753075f31e562401755e3d0
jim <wksunshine@gmail.com>
2024-07-08 16:42:50 +1000

    Finish the landing page including the footer

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -75,7 +75,7 @@ interface ReviewProps extends HTMLAttributes<HTMLDivElement> {
 function Review({imgSrc, className, ...props}: ReviewProps) {
   const POSSIBLE_ANIMATION_DELAYS = ['0s', '0.1s', '0.2s', '0.3s', '0.4s', '0.5s']
 
-  const animationDelay = POSSIBLE_ANIMATION_DELAYS[Math.random() * POSSIBLE_ANIMATION_DELAYS.length];
+  const animationDelay = POSSIBLE_ANIMATION_DELAYS[Math.floor(Math.random() * POSSIBLE_ANIMATION_DELAYS.length)];
 
   return <div 
     className={cn('animate-fade-in rounded-[2.25rem] bg-white p-6 opacity-0 shadow-xl shadow-slate-900/5', className)} 
@@ -117,6 +117,8 @@ function ReviewGrid() {
         msPerPixel={10}
       />
     </>: null}
+    <div className="pointer-events-none absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-slate-100" />
+    <div className="pointer-events-none absolute inset-x-0 bottom-0 h-32 bg-gradient-to-t from-slate-100" />
   </div>
 }
 
