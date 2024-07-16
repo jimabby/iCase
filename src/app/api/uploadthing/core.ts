421da3fff5a1ab7501ee46ad60ea382aa85dcd96
jim <wksunshine@gmail.com>
2024-07-16 17:40:05 +1000

    Configurator Page

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,5 +1,7 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import {z} from 'zod';
+import sharp from 'sharp';
+import { db } from "@/db";
  
 const f = createUploadthing();
  
@@ -21,8 +23,34 @@ export const ourFileRouter = {
       console.log("file url", file.url);
  
       const { configId } = metadata.input;
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-      return { configId };
+
+      const res = await fetch(file.url);
+      const buffer = await res.arrayBuffer();
+
+      const imgMetadata = await sharp(buffer).metadata()
+      const {width, height} = imgMetadata
+
+      if(!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            height: height || 500,
+            width: width || 500
+          }
+        })
+        return {configId: configuration.id}
+      }else {
+        const updatedConfiguration = await db.configuration.update({
+          where: {
+            id: configId
+          },
+          data: {
+            croppedImageUrl: file.url,
+          }
+        })
+
+        return {configId: updatedConfiguration.id}
+      }
     }),
 } satisfies FileRouter;
  
