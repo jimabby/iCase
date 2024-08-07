ab569ba73b74c8b94eb509bef9596a08d15a450b
jim <wksunshine@gmail.com>
2024-08-08 02:10:25 +1000

    bug fix

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -16,51 +16,53 @@ export async function POST(req: Request){
     const event = stripe.webhooks.constructEvent(body, signature, process.env.STRIPE_WEBHOOK_SECRET!);
 
     if(event.type === "checkout.session.completed") {
-      throw new Error('Missing user email!');
-    }
+      if(!event.data.object.customer_details?.email){
+        throw new Error('Missing user email!');
+      }
 
-    const session = event.data.object as Stripe.Checkout.Session;
+      const session = event.data.object as Stripe.Checkout.Session;
 
-    const { userId, orderId } = session.metadata || {
-      userId: null,
-      orderId: null
-    }
+      const { userId, orderId } = session.metadata || {
+        userId: null,
+        orderId: null
+      }
 
-    if(!userId || !orderId) {
-      throw new Error('Invalid request metadata!');
-    }
+      if(!userId || !orderId) {
+        throw new Error('Invalid request metadata!');
+      }
 
-    const billingAddress = session.customer_details!.address;
-    const shippingAddress = session.shipping_details!.address;
+      const billingAddress = session.customer_details!.address;
+      const shippingAddress = session.shipping_details!.address;
 
-    await db.order.update({
-      where: {
-        id: orderId
-      },
-      data: {
-        isPaid: true,
-        shippingAddress: {
-          create: {
-            name: session.customer_details!.name!,
-            city: shippingAddress!.city!,
-            country: shippingAddress!.country!,
-            postalCode: shippingAddress!.postal_code!,
-            street: shippingAddress!.line1!,
-            state: shippingAddress!.state
-          }
+      await db.order.update({
+        where: {
+          id: orderId
         },
-        billingAddress: {
-          create: {
-            name: session.customer_details!.name!,
-            city: billingAddress!.city!,
-            country: billingAddress!.country!,
-            postalCode: billingAddress!.postal_code!,
-            street: billingAddress!.line1!,
-            state: billingAddress!.state
+        data: {
+          isPaid: true,
+          shippingAddress: {
+            create: {
+              name: session.customer_details!.name!,
+              city: shippingAddress!.city!,
+              country: shippingAddress!.country!,
+              postalCode: shippingAddress!.postal_code!,
+              street: shippingAddress!.line1!,
+              state: shippingAddress!.state
+            }
+          },
+          billingAddress: {
+            create: {
+              name: session.customer_details!.name!,
+              city: billingAddress!.city!,
+              country: billingAddress!.country!,
+              postalCode: billingAddress!.postal_code!,
+              street: billingAddress!.line1!,
+              state: billingAddress!.state
+            }
           }
         }
-      }
-    })
+      })
+    }   
 
     return NextResponse.json({result: event, ok: true});
 
